14404b1a2e36a06d9e03e9e7a707e7ab0530593d
imwh0im <48340699+imwh0im@users.noreply.github.com>
2025-05-01 19:56:36 +0900

    gpt 를 이용한 뉴스 요약

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const puppeteer = require('puppeteer');
+const { OpenAI } = require('openai');
+
+const openAi = new OpenAI({
+  apiKey: 'API Key',
+});
 
 const naverNewsUrl = 'https://news.naver.com/section/100';
 
@@ -36,7 +41,23 @@ const aTagElement = 'body > div > div#ct_wrap > div.ct_scroll_wrapper > div#news
       '#dic_area',
       el => el.innerText,
     );
-    newsArray.push({ title, content });
+    newsArray.push({ title, content, link });
   }
   console.log(newsArray);
+
+  // 4. ChatGPT 에게 요약 요청
+  const response = await openAi.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    messages: [
+      { role: 'system', content: 'title, content, link 오브젝트로 이루어진 배열을 전달합니다.' },
+      { role: 'system', content: 'title 은 뉴스의 제목, content 는 뉴스의 내용, link 는 뉴스의 원본 링크 입니다.' },
+      { role: 'system', content: '각 뉴스별로 요약하여, markdown 파일을 주세요.' },
+      { role: 'system', content: '요약 방법은 다음과 같습니다. 뉴스 제목, 요약 내용, 원본 뉴스 링크, 주요 키워드 입니다.' },
+      { role: 'system', content: '요약 내용 외의 다른 응답을 해주시지 말아 주세요.' },
+      { role: 'user', content: JSON.stringify(newsArray) },
+    ],
+    temperature: 0.4,
+  });
+
+  console.log(response.choices[0].message.content);
 })();
